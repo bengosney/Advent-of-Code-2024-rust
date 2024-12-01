9ec87baacbd01c56b43f419d6caa9c9fafc79203
Ben Gosney <bengosney@googlemail.com>
2024-12-01 21:21:33 +0000

    build(makefile): simple make file

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,4 +1,11 @@
+/* [[[cog
+import cog
+from pathlib import Path
+for day in sorted([p.name.replace(".rs", "") for p in list(Path("./rust/src").glob("day*.rs"))]):
+   cog.outl(f"mod {day};")
+]]]*/
 mod day01;
+/*[[[end]]] (checksum: 909f8627da7e00e70db3ecbb5ee3ebb0) */
 
 fn main() {
     println!("Hello, world!");
