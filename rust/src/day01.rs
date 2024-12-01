64244377c0325228ead7fb791d4fccbc10094176
Ben Gosney <bengosney@googlemail.com>
2024-12-01 20:29:39 +0000

    feat(day02): day 2 and a refactor

--- rust/src/day01.rs
+++ rust/src/day01.rs
@@ -1,5 +1,6 @@
 use std::iter::zip;
 
+#[cfg(test)]
 const EXAMPLE_INPUT: &str = r#"3   4
 4   3
 2   5
@@ -13,26 +14,38 @@ fn test_part1_example() {
     assert_eq!(part1, Result::Ok(11));
 }
 
+#[test]
+fn test_part2_example() {
+    let part2 = part2(EXAMPLE_INPUT);
+    assert_eq!(part2, Result::Ok(31));
+}
+
 fn parse_input(input: &str) -> (Vec<i32>, Vec<i32>) {
-    let mut left = Vec::new();
-    let mut right = Vec::new();
-    for line in input.lines() {
-        let numbers: Vec<i32> =
-            line.split_whitespace().map(|s| s.parse::<i32>().unwrap()).collect();
-        left.push(numbers[0]);
-        right.push(numbers[1]);
-    }
-    left.sort();
-    right.sort();
-    
-    (left, right)
+    input
+        .lines()
+        .map(|line| {
+            let numbers: Vec<i32> = line
+                .split_whitespace()
+                .map(|s| s.parse::<i32>().unwrap())
+                .collect();
+            (numbers[0], numbers[1])
+        })
+        .unzip()
 }
 
 pub fn part1(input: &str) -> Result<i32, &'static str> {
-    let mut sum = 0;
+    let (mut left, mut right) = parse_input(input);
+
+    left.sort();
+    right.sort();
+
+    Ok(zip(left, right).map(|(l, r)| (r - l).abs()).sum())
+}
+
+pub fn part2(input: &str) -> Result<i32, &'static str> {
     let (left, right) = parse_input(input);
-    for (l,r) in zip(left, right) {
-        sum += (r - l).abs();
-    }
-    Ok(sum)
+
+    let count_items = |what: i32| right.iter().filter(|&r| *r == what).count();
+
+    Ok(left.iter().map(|l| l * count_items(*l) as i32).sum())
 }
