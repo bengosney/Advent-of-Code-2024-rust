a0f47184b5ed0f3a7f2a46cc7cbe01041103b9e8
Ben Gosney <bengosney@googlemail.com>
2024-12-11 20:52:57 +0000

    refactor(day11): use the cached crate

--- src/day11.rs
+++ src/day11.rs
@@ -1,5 +1,5 @@
 use crate::utils::PuzzleResult;
-use std::collections::HashMap;
+use cached::proc_macro::cached;
 
 #[cfg(test)]
 const EXAMPLE_INPUT: &str = r#"125 17"#;
@@ -18,47 +18,39 @@ fn parse_input(input: &str) -> Vec<i64> {
         .collect()
 }
 
-fn blink(stone: i64, iterations: i32, cache: &mut HashMap<(i64, i32), i64>) -> i64 {
+#[cached]
+fn blink(stone: i64, iterations: i32) -> i64 {
     if iterations == 0 {
         return 1;
     }
 
-    if let Some(&result) = cache.get(&(stone, iterations)) {
-        return result;
-    }
-
     let value = if stone == 0 {
-        blink(1, iterations - 1, cache)
+        blink(1, iterations - 1)
     } else {
         let stone_str = stone.to_string();
         if stone_str.len() % 2 == 0 {
             let mid = stone_str.len() / 2;
             let (first_half, second_half) = stone_str.split_at(mid);
-            blink(first_half.parse().unwrap(), iterations - 1, cache)
-                + blink(second_half.parse().unwrap(), iterations - 1, cache)
+            blink(first_half.parse().unwrap(), iterations - 1)
+                + blink(second_half.parse().unwrap(), iterations - 1)
         } else {
-            blink(stone * 2024, iterations - 1, cache)
+            blink(stone * 2024, iterations - 1)
         }
     };
 
-    cache.insert((stone, iterations), value);
     value
 }
 
 pub fn part1(input: &str) -> PuzzleResult {
-    let mut blink_cache: HashMap<(i64, i32), i64> = HashMap::new();
-
     Ok(parse_input(input)
         .into_iter()
-        .map(|stone| blink(stone, 25, &mut blink_cache))
+        .map(|stone| blink(stone, 25))
         .sum::<i64>() as usize)
 }
 
 pub fn part2(input: &str) -> PuzzleResult {
-    let mut blink_cache: HashMap<(i64, i32), i64> = HashMap::new();
-
     Ok(parse_input(input)
         .into_iter()
-        .map(|stone| blink(stone, 75, &mut blink_cache))
+        .map(|stone| blink(stone, 75))
         .sum::<i64>() as usize)
 }
